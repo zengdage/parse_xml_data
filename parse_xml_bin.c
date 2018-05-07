a76d4eaed6f9a4df77a95bb98518ceb96f3b8864
zengzhijin <zhijinzeng@allwinnertech.com>
2018-05-07 08:56:13 +0800

    add check_magic

--- parse_xml_bin.c
+++ parse_xml_bin.c
@@ -16,6 +16,29 @@ __s8 * load_bin(const char * path);
 
 __s32 find_view(const char * win_id, void * bin_data, view_table_item_t view_item);
 
+__s32 check_magic(void * bin_data);
+
+__s32 check_magic(void * bin_data)
+{
+	data_file_header_t header = (data_file_header_t) bin_data;
+	printf("magic = %s \nxml_num = %d \nview_num = %d \nxml_view_table_pos = %d\n",
+					header->magic,
+					header->xml_num,
+					header->view_num,
+					header->xml_view_table_pos);
+	__s8 * magic = MAGIC;
+	if(strcmp(header->magic, magic))
+	{
+		printf("check magic failed!\n");
+		return -1;
+	}
+	return 0;
+}
+
+__s32 find_view(const char * win_id, void * bin_data, view_table_item_t view_item)
+{
+	return 0;	
+}
 
 __s8 * load_bin(const char * path)
 {
@@ -48,11 +71,17 @@ int main()
     
     __s8 * bin_path = "view.data";
     bin_data = load_bin(bin_path);
-    if(bin_data != 0)
+    if(bin_data == 0)
     {
-        printf("load_bin failed");
+        printf("load_bin failed\n");
         return -1;
     }
+
+	ret = check_magic(bin_data);
+	if(ret != 0)
+	{
+		return -1;
+	}
 
     printf("parse xml data end\n");
 }
